3cd053c0a189bbf43ab42ca210e0a5c05d179be2
AidenChang <asdf62210@gmail.com>
2025-07-16 14:18:48 +0800

    新增畫面的title

--- app/src/main/java/com/example/snoozelooapp/alarm/list/presentation/AlarmListScreenRoot.kt
+++ app/src/main/java/com/example/snoozelooapp/alarm/list/presentation/AlarmListScreenRoot.kt
@@ -1,19 +1,52 @@
 package com.example.snoozelooapp.alarm.list.presentation
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import org.koin.androidx.compose.koinViewModel
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 /**
  * Created by AidenChang on 2025/7/15
  */
 @Composable
 fun AlarmListScreenRoot(
-    viewModel: AlarmListViewModel = koinViewModel()
+//    viewModel: AlarmListViewModel = koinViewModel()
 ) {
     AlarmListScreen()
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun AlarmListScreen() {
-
+fun AlarmListScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier = modifier
+    ) { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ) {
+            stickyHeader {
+                Surface(
+                    color = MaterialTheme.colorScheme.background.copy(alpha = 0.8f),
+                ) {
+                    Text(
+                        text = "Your Alarms",
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier
+                            .padding(vertical = 8.dp, horizontal = 16.dp)
+                    )
+                }
+            }
+        }
+    }
 }
