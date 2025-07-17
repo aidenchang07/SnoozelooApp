e466ccf931866d0a23ea48f00858a611397f42c7
AidenChang <asdf62210@gmail.com>
2025-07-17 14:32:32 +0800

    新增FloatingActionButton

--- app/src/main/java/com/example/snoozelooapp/alarm/list/presentation/AlarmListScreenRoot.kt
+++ app/src/main/java/com/example/snoozelooapp/alarm/list/presentation/AlarmListScreenRoot.kt
@@ -2,8 +2,11 @@ package com.example.snoozelooapp.alarm.list.presentation
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
@@ -12,12 +15,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.FloatingActionButtonElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -25,6 +37,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.snoozelooapp.R
+import com.example.snoozelooapp.core.extensions.noRippleClickable
 
 /**
  * Created by AidenChang on 2025/7/15
@@ -43,8 +56,26 @@ fun AlarmListScreen(
 ) {
     val topDp = WindowInsets.systemBars.asPaddingValues().calculateTopPadding()
     Scaffold(
-        modifier = modifier
-            .fillMaxSize()
+        modifier = modifier.fillMaxSize(),
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            FloatingActionButton(
+                shape = CircleShape,
+                containerColor = MaterialTheme.colorScheme.primary,
+                elevation = FloatingActionButtonDefaults.elevation(0.dp),
+                onClick = {}
+            ) {
+                Icon(
+                    modifier = Modifier.noRippleClickable {
+                        // 實作這邊功能
+                        println("aiden inner Icon")
+                    },
+                    imageVector = Icons.Default.Add,
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                    contentDescription = null
+                )
+            }
+        }
     ) { padding ->
         Box(
             modifier = Modifier
