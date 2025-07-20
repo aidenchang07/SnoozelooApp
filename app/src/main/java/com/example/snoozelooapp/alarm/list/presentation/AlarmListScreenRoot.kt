f98b607d65d4bb715a159d29c836f6893142c27f
AidenChang <asdf62210@gmail.com>
2025-07-20 19:24:39 +0800

    基礎Item架構

--- app/src/main/java/com/example/snoozelooapp/alarm/list/presentation/AlarmListScreenRoot.kt
+++ app/src/main/java/com/example/snoozelooapp/alarm/list/presentation/AlarmListScreenRoot.kt
@@ -2,11 +2,12 @@ package com.example.snoozelooapp.alarm.list.presentation
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
@@ -15,26 +16,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.FloatingActionButtonElevation
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.snoozelooapp.R
 import com.example.snoozelooapp.core.extensions.noRippleClickable
@@ -105,6 +108,85 @@ fun AlarmListScreen(
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+private fun AlarmItem() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Top
+        ) {
+            Column {
+                Text(
+                    text = "Wake Up",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = "10:00",
+                        style = MaterialTheme.typography.displayLarge
+                    )
+                    Text(
+                        modifier = Modifier.alignByBaseline(),
+                        text = "AM",
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                }
+                Text(
+                    text = "Alarm in 30min",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+            Switch(
+                checked = false,
+                onCheckedChange = {
+                    // 實作click功能
+                }
+            )
+        }
+        FlowRow {
+            Text("Mon")
+            Text("Tue")
+            Text("Wed")
+            Text("Thu")
+            Text("Fri")
+            Text("Sat")
+            Text("Sun")
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Go to bed at 02:00AM to get 8h of sleep",
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.secondary
+            )
+            IconButton(
+                onClick = {
+                    // 實作click功能
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    tint = Color.Red,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
+
 @Composable
 private fun EmptyAlarmListContent(
     modifier: Modifier = Modifier
@@ -129,4 +211,10 @@ private fun EmptyAlarmListContent(
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AlarmItemPreview() {
+    AlarmItem()
 }
