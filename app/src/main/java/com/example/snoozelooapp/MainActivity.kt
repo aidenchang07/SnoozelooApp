68dc886baeb2b1d0b956983477d536f77aa4d188
AidenChang <asdf62210@gmail.com>
2025-07-22 11:27:45 +0800

    SplashScreen結束後套用原先主題，ActionBar才不會跑出來
    注意：SplashScreen結束後更換主題需搭配installSplashScreen()才有效果

--- app/src/main/java/com/example/snoozelooapp/MainActivity.kt
+++ app/src/main/java/com/example/snoozelooapp/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.snoozelooapp.alarm.list.presentation.AlarmListScreenRoot
 import com.example.snoozelooapp.core.designsystem.theme.SnoozelooAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
